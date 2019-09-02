e37c02d2b0276f2dfadab257d47c152b01711558
ZakharTymchenko <ZakharTymchenko@github.com>
2019-09-02 15:00:03 +0200

    updated mac build script

--- install_mac.sh
+++ install_mac.sh
@@ -4,4 +4,6 @@ brew install cmake openblas lapack armadillo boost
 chmod u+x mac_install_mlpack.sh
 ./mac_install_mlpack.sh
 brew install R gnuplot
+brew install python2
+pip2 install numpy scipy pandas sklearn tensorflow
 python mac_build.py
