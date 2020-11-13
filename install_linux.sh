9883cf228d6674a411685206746ef1304858021f
Zakhar Tymchenko <TheDeadSkin@hotmail.ch>
2020-11-13 09:39:07 +0100

    Update install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -1,5 +1,5 @@
 sudo apt-get install build-essential cmake libopenmpi-dev libopenblas-dev liblapack-dev libarmadillo-dev
-sudo apt-get install octave-pkg-dev r-base gnuplot
+sudo apt-get install r-base gnuplot
 sudo apt install python-dev
 sudo apt install python-pip
 pip2 install numpy scipy pandas sklearn
