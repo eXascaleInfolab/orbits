2d8059da9ef54b575343e2220ca51526bb45859d
Zakhar Tymchenko <TheDeadSkin@hotmail.ch>
2021-08-12 13:05:34 +0200

    Update install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -1,7 +1,11 @@
-sudo apt-get install build-essential cmake libopenmpi-dev libopenblas-dev liblapack-dev libarmadillo-dev
-sudo apt-get install r-base gnuplot
-sudo apt install python-dev
-sudo apt install python-pip
+sudo apt-get -y update
+suod apt-get -y upgrade
+sudo apt-get -y install build-essential cmake libopenmpi-dev libopenblas-dev liblapack-dev libarmadillo-dev
+sudo apt-get -y install r-base gnuplot
+sudo apt-get -y install python-dev
+curl "https://bootstrap.pypa.io/pip/2.7/get-pip.py" -o get-pip.py
+sudo python get-pip.py
+rm get-pip.py
 pip2 install numpy scipy pandas sklearn
-pip2 install tensorflow
+pip2 install tensorflow==1.14.0
 python linux_build.py
